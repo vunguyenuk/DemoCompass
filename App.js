b7b450f29e97d9fb3055ae2ea19764df2d6ca5b7
Vunguyen <vunv.uk@gmail.com>
2018-11-02 08:58:23 +0700

    first init

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Text, View, Animated, Easing } from 'react-native';
+import Compass from './components/Compass.js'
+
+
+export default class App extends Component<Props> {
+  state = {
+    fadeAnim: new Animated.Value(0)
+  }
+  
+  componentDidMount() {
+      // Fade in on app launch
+      Animated.timing(
+        this.state.fadeAnim,
+        {
+          toValue: 1,
+          duration: 1200,
+          easing: Easing.linear
+        }
+      ).start();
+    }
+  
+  render() {
+    return (
+      <Animated.View style={
+        {
+        flex: 1,
+        backgroundColor: "#ecedef",
+        opacity: this.state.fadeAnim
+        }
+      } >
+        <Compass />
+      </Animated.View>
+    );
+  }
+  }
